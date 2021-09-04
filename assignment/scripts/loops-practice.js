07f6b89740989eff3695b72656cd28e5ef4fb372
Julia Bayless <julia.allistair@gmail.com>
2021-09-04 10:40:48 -0500

    Finished assignment 2

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -58,6 +58,19 @@ while (stars > 5){
 }
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
+let index = 0;
+let max = 5;
+while (index <= max){
+  console.log( index );
+  index++;
+}
 
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
+
+index = 10;
+let min = 5;
+while (index >= min) {
+  console.log( index );
+  index--;
+}
